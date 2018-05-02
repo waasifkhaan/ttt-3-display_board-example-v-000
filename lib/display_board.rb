6a6179358b5f99b4f3f4ea59a578ebedd8b8dd1e
Wasif Khan <waasif.khaan@gmail.com>
2018-05-02 22:27:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,5 +5,5 @@
    puts "-----------"
    puts " #{board[3]} | #{board[4]} | #{board[5]} "
    puts "-----------"
-   print " #{board[6]} | #{board[7]} | #{board[8]} "
+   puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
