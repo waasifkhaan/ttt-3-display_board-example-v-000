bd2cd52cbd9e53b17791468cd76ef9dec5e62910
Wasif Khan <waasif.khaan@gmail.com>
2018-05-02 22:03:03 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,7 +3,7 @@
    board = [" "," "," "," "," "," "," "," "," "]
    print ""board[0]","|","board[1]","|","board[2]""
    puts "-----------"
-   print ["board[3]","|","board[4]","|","board[5]"]
+   print ""board[3]","|","board[4]","|","board[5]""
    puts "-----------"
    print ""board[6]","|","board[7]","|","board[8]""
  end
